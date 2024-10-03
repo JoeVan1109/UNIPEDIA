255c136a5e40f5f86e0b5d630fc6476d4970f2c0
CharlieVan <charlie.van1109@gmail.com>
2024-10-03 13:21:28 +0200

    ajout js et travail banniere product page

---
+++ JS/product-page.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let title = document.querySelector(".title-product-peluche");
+    let imgPeluche = document.querySelector(".banniere-img-peluche");
+
+    title.classList.add("active-title");
+    imgPeluche.classList.add("active-banniere");
+});

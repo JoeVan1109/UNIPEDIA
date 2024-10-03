3d4cfabdeb162c101811c1e7d335b01d006a3715
CharlieVan <charlie.van1109@gmail.com>
2024-10-03 18:26:34 +0200

    card peluche faite manque prix + icone panier, repsonssive ok

--- JS/product-page.js
+++ JS/product-page.js
@@ -9,26 +9,5 @@ function banner () {
     });
 }
 
-function product() {
-    const cards = document.querySelectorAll('.card-1, .card-2, .card-3, .card-4');
-    
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('card-peluche-active');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1 // Déclenche quand 10% de la carte est visible
-    });
-
-    cards.forEach(card => observer.observe(card));
-}
-
-// Appeler la fonction au chargement de la page
-document.addEventListener('DOMContentLoaded', product);
 
 banner();

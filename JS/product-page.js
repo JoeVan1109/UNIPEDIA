e0aa224e3542171a665b560cf30d1b97269620f0
CharlieVan <charlie.van1109@gmail.com>
2024-10-03 18:18:02 +0200

    avancement product page peluche avec card de product

--- JS/product-page.js
+++ JS/product-page.js
@@ -1,7 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let title = document.querySelector(".title-product-peluche");
-    let imgPeluche = document.querySelector(".banniere-img-peluche");
+function banner () {
 
-    title.classList.add("active-title");
-    imgPeluche.classList.add("active-banniere");
-});
+    document.addEventListener("DOMContentLoaded", function () {
+        let title = document.querySelector(".title-product-peluche");
+        let imgPeluche = document.querySelector(".banniere-img-peluche");
+    
+        title.classList.add("active-title");
+        imgPeluche.classList.add("active-banniere");
+    });
+}
+
+function product() {
+    const cards = document.querySelectorAll('.card-1, .card-2, .card-3, .card-4');
+    
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('card-peluche-active');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1 // Déclenche quand 10% de la carte est visible
+    });
+
+    cards.forEach(card => observer.observe(card));
+}
+
+// Appeler la fonction au chargement de la page
+document.addEventListener('DOMContentLoaded', product);
+
+banner();

33b02e66863d22c8f9df3d4af3b956db5a9816e7
CharlieVan <charlie.van1109@gmail.com>
2024-10-04 13:08:28 +0200

    mise en place banner encyclo + ajout lien product page

--- JS/anime-banniere.js
+++ JS/anime-banniere.js
@@ -1,13 +1,15 @@
 function banner () {
 
     document.addEventListener("DOMContentLoaded", function () {
-        let title = document.querySelector(".title-product-peluche");
-        let imgPeluche = document.querySelector(".banniere-img-peluche");
+        let title = document.querySelector(".title-banniere");
+        let imgPeluche = document.querySelector(".banniere-img");
     
         title.classList.add("active-title");
         imgPeluche.classList.add("active-banniere");
     });
-}
+};
+
+
 
 
 banner();

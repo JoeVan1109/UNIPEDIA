bf69878996b2d7d78467eb61b9aa988ae17a65f7
Charlie <clarisse.vanlerberghe@gmail.com>
2024-12-22 10:32:55 +0100

    comment js

--- front/JS/anime-banniere.js
+++ front/JS/anime-banniere.js
@@ -1,15 +1,18 @@
+// Function to animate the banner display
 function banner () {
-
+    // Wait for the DOM to be fully loaded
     document.addEventListener("DOMContentLoaded", function () {
+        // Select the title element with the class "title-banniere"
         let title = document.querySelector(".title-banniere");
+        // Select the image element with the class "banniere-img"
         let imgPeluche = document.querySelector(".banniere-img");
     
+        // Add the class "active-title" to the title element to activate its animation
         title.classList.add("active-title");
+        // Add the class "active-banniere" to the image element to activate its animation
         imgPeluche.classList.add("active-banniere");
     });
 };
 
-
-
-
+// Call the banner function to execute the animation
 banner();
--- front/JS/navbar.js
+++ front/JS/navbar.js
@@ -1,37 +1,39 @@
-// Menu burger
+// Burger menu
 
+// Retrieving html elements for use in Javascript
 const sidenav = document.getElementById("mySidenav");
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 
+// Function to open the side navigation
 function openNav() {
     sidenav.classList.add("active");
 }
 
+// Function to close the side navigation
 function closeNav() {
     sidenav.classList.remove("active");
 }
 
+// Add event listeners to open and close buttons
 openBtn.addEventListener("click", openNav);
 closeBtn.addEventListener("click", closeNav);
-
 
 const navEl = document.querySelector('.bandeau');
 const navLinksEl = document.querySelectorAll('.nav-link');
 
-let lastScrollTop = 0; // Variable pour stocker la position précédente du scroll
+let lastScrollTop = 0; // Variable to store the previous scroll position
 
+// Add event listener for window scroll
 window.addEventListener('scroll', () => {
-
-    const scrollTop = window.scrollY;
-    // Position actuelle du scroll
+    const scrollTop = window.scrollY; // Current scroll position
     if (scrollTop > lastScrollTop) {
-        // Scroll vers le bas
+        // Scrolling down
         navEl.classList.add('navbar-scrolled');
     } else {
-        // Scroll vers le haut
+        // Scrolling up
         navEl.classList.remove('navbar-scrolled');
     }
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Met à jour la position précédente du scroll
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Update the previous scroll position
 });
 
